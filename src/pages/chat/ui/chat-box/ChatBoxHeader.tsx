b1e74c722de31225d224185e7d764d6b646226ca
Noikstrax <noikstrax@tuta.io>
2025-08-29 13:22:14 +0300

    fix(chatBoxHeader): fix select model button

---
+++ src/pages/chat/ui/chat-box/ChatBoxHeader.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/shared/ui/dropdown-menu";
+import { selectModel } from "../../../../../app/store/models/modelsSlice";
+import { useAppDispatch, useAppSelector } from "../../../../../app/store/hooks";
+
+export const ChatBoxHeader = () => {
+  const dispatch = useAppDispatch();
+  const models = useAppSelector((state) => state.models);
+  const selectedModel = models.find((model) => model.isSelected);
+
+  const getModels = async () => {
+    const res = await fetch("/api/models");
+    const models = await res.json();
+    console.log(models);
+  };
+
+  return (
+    <div className="ml-3">
+      <DropdownMenu>
+        <DropdownMenuTrigger className="rounded-sm hover:bg-gray-500 px-1 py-1 hover:cursor-pointer">
+          {selectedModel?.name ?? "Model is not selected"}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Models</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {models.map((model) => (
+            <DropdownMenuItem
+              key={model.name}
+              className="rounded-sm hover:bg-gray-500 px-1 py-1 hover:cursor-pointer"
+              onClick={() =>
+                dispatch(
+                  selectModel({
+                    name: model.name,
+                  })
+                )
+              }
+            >
+              <button className="hover:cursor-pointer">
+                {model.name} {model.isSelected ? "+" : ""}
+              </button>
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
