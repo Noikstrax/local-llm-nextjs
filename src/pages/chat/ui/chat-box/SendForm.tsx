07baeb38f77d6daf76479529d9f0d543986dec84
Noikstrax <noikstrax@tuta.io>
2025-09-04 17:23:54 +0300

    feat(sendForm): replace input with textarea and add auto-resize

--- src/pages/chat/ui/chat-box/SendForm.tsx
+++ src/pages/chat/ui/chat-box/SendForm.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useEffect, useRef } from "react";
+import { text } from "stream/consumers";
+
 interface Props {
   handleSend: () => void;
   newMessage: string;
@@ -7,6 +10,43 @@ interface Props {
 }
 
 export const SendForm = ({ handleSend, newMessage, onChange }: Props) => {
+  const adjustHeight = (ta: HTMLTextAreaElement) => {
+    ta.style.height = "auto";
+
+    const style = window.getComputedStyle(ta);
+
+    let lineHeight = parseFloat(style.lineHeight);
+
+    if (isNaN(lineHeight)) {
+      const fontSize = parseFloat(style.fontSize);
+      lineHeight = fontSize * 1.2;
+    }
+
+    const maxHeight = lineHeight * 8;
+
+    ta.style.height = Math.min(ta.scrollHeight, maxHeight) + "px";
+    ta.style.overflowY = ta.scrollHeight > maxHeight ? "auto" : "hidden";
+  };
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(e.target.value);
+    if (!textareaRef.current) return;
+    adjustHeight(e.target);
+  };
+
+  useEffect(() => {
+    if (!textareaRef.current) return;
+
+    const observer = new ResizeObserver(() => {
+      adjustHeight(textareaRef.current!);
+    });
+    observer.observe(textareaRef.current);
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <form
       onSubmit={(e) => {
@@ -15,17 +55,27 @@ export const SendForm = ({ handleSend, newMessage, onChange }: Props) => {
       }}
       className="flex space-between w-full mt-10"
     >
-      <input
-        className="border px-2 py-1 rounded w-full"
-        value={newMessage}
-        onChange={(e) => onChange(e.target.value)}
-      />
-      <button
-        className="bg-blue-500 text-white px-4 py-1 rounded cursor-pointer"
-        type="submit"
-      >
-        Send
-      </button>
+      <div className="relative grid grid-cols-1 w-full bg-gray-500 py-2 px-2 rounded-xl">
+        <div>
+          <textarea
+            id="input"
+            className="border px-2 py-1 rounded w-full border-none outline-none resize-none"
+            value={newMessage}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            ref={textareaRef}
+          />
+        </div>
+        <div className="flex justify-end">
+          <button
+            className="bg-blue-500 text-white px-4 py-1 rounded cursor-pointer row-end-1"
+            type="submit"
+          >
+            Send
+          </button>
+        </div>
+      </div>
     </form>
   );
 };
