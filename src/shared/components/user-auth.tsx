bbad4c640f550e1cdce156315d3bb72355298408
Noikstrax <noikstrax@tuta.io>
2025-10-07 11:35:17 +0300

    remove unused imports

--- src/shared/components/user-auth.tsx
+++ src/shared/components/user-auth.tsx
@@ -1,11 +1,6 @@
-import { useSession, signIn } from "next-auth/react";
-import Link from "next/link";
-import { Button } from "../ui";
 import { ProfileButton } from "./profile-button";
 
 export const UserAuth = () => {
-  const { data: session } = useSession();
-  console.log(session, 999);
   return (
     <div>
       <ProfileButton />
