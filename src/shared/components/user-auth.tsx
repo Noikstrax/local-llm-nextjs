f1d0ba453a9c7653630b64a01e7da93caf7e197d
Noikstrax <noikstrax@tuta.io>
2025-10-13 18:26:02 +0300

    feat(auth): move profile-button component logic to user-auth component

--- src/shared/components/user-auth.tsx
+++ src/shared/components/user-auth.tsx
@@ -1,9 +1,23 @@
-import { ProfileButton } from "./profile-button";
+import { useSession, signOut } from "next-auth/react";
+import Link from "next/link";
+import { Button } from "../ui";
 
 export const UserAuth = () => {
+  const { data: session } = useSession();
   return (
     <div>
-      <ProfileButton />
+      {!session ? (
+        <>
+          <Link href={"/login"}>Login</Link>
+        </>
+      ) : (
+        <>
+          <div>Logged</div>
+          <Button variant="outline" onClick={() => signOut()}>
+            Logout
+          </Button>
+        </>
+      )}
     </div>
   );
 };
