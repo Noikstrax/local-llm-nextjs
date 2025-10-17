98f6c7445a1f07cded0731f19e3eeac8b770fbfc
Noikstrax <noikstrax@tuta.io>
2025-10-17 12:45:59 +0300

    feat(message): change styles for pre and code tags

--- src/pages/chat/ui/chat-box/messages-list/SuccessfulMessage.tsx
+++ src/pages/chat/ui/chat-box/messages-list/SuccessfulMessage.tsx
@@ -10,17 +10,30 @@ export const SuccessfulMessage = ({ message, isAi }: Props) => {
   return (
     <div
       key={message.id}
-      className={`
-        my-5
-        rounded-xl
-        max-w-full
-        text-justify
-        prose prose-invert
-        ${
+      className={
+        "my-5 rounded-xl max-w-full text-justify prose prose-invert " +
+        `${
           isAi ? "self-start" : "self-end bg-zinc-800 px-5 py-3 rounded-xl my-5"
-        }`}
+        }`
+      }
     >
-      <ReactMarkdown>{message.text}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          pre: ({ node, ...props }: any) => (
+            <pre
+              className="max-w-full overflow-x-auto bg-gray-700 p-3 rounded-lg mb-2 whitespace-pre-wrap"
+              {...props}
+            />
+          ),
+          code: ({ node, inline, className, children, ...props }: any) => (
+            <code className={"bg-gray-700 px-2 py-0.5"} {...props}>
+              {children}
+            </code>
+          ),
+        }}
+      >
+        {message.text}
+      </ReactMarkdown>
     </div>
   );
 };
