9273bc36e764f178ab6846c5941f45cc6ef80dcd
Noikstrax <noikstrax@tuta.io>
2025-10-17 13:33:53 +0300

    feat(chat): add code highlighting for AI answers

--- src/pages/chat/ui/chat-box/messages-list/SuccessfulMessage.tsx
+++ src/pages/chat/ui/chat-box/messages-list/SuccessfulMessage.tsx
@@ -1,5 +1,8 @@
 import ReactMarkdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css"; // подключаем тему
 import { Message } from "../MessageList";
+import "./code.css";
 
 interface Props {
   message: Message;
@@ -10,23 +13,21 @@ export const SuccessfulMessage = ({ message, isAi }: Props) => {
   return (
     <div
       key={message.id}
-      className={
-        "my-5 rounded-xl max-w-full text-justify prose prose-invert " +
-        `${
-          isAi ? "self-start" : "self-end bg-zinc-800 px-5 py-3 rounded-xl my-5"
-        }`
-      }
+      className={`my-5 rounded-xl max-w-full text-justify prose prose-invert ${
+        isAi ? "self-start" : "self-end bg-zinc-800 px-5 py-3 rounded-xl my-5"
+      }`}
     >
       <ReactMarkdown
+        rehypePlugins={[rehypeHighlight]}
         components={{
           pre: ({ node, ...props }: any) => (
             <pre
-              className="max-w-full overflow-x-auto bg-gray-700 p-3 rounded-lg mb-2 whitespace-pre-wrap"
+              className="bg-gray-700 p-3 rounded-lg overflow-hidden mb-2 break-words"
               {...props}
             />
           ),
           code: ({ node, inline, className, children, ...props }: any) => (
-            <code className={"bg-gray-700 px-2 py-0.5"} {...props}>
+            <code className="bg-gray-700 px-2 py-0.5 rounded" {...props}>
               {children}
             </code>
           ),
