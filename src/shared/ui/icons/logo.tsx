a9373bcaf00d48fcb22a9d20046a7b5b440585b5
Noikstrax <noikstrax@tuta.io>
2025-10-27 19:11:08 +0300

    feat(ui): allow custom text size for logo letters

--- src/shared/ui/icons/logo.tsx
+++ src/shared/ui/icons/logo.tsx
@@ -2,13 +2,14 @@ import styles from "./icons.module.css";
 
 interface Props {
   text: string;
+  className?: string;
 }
 
-export const Logo = ({ text }: Props) => {
+export const Logo = ({ text, className }: Props) => {
   return (
     <div className={styles.logo}>
       <div>
-        <p>{text}</p>
+        <p className={className || ""}>{text}</p>
       </div>
     </div>
   );
