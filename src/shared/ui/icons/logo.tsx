d71f1f85235714b11efd263e12e0fd6093b1da57
cyntraten <noikstrax@tuta.io>
2025-11-20 23:09:48 +0300

    "Add props for conditional rendering app icon and profile icon in LeftBar"

--- src/shared/ui/icons/logo.tsx
+++ src/shared/ui/icons/logo.tsx
@@ -3,14 +3,25 @@ import styles from "./icons.module.css";
 interface Props {
   text: string;
   className?: string;
+  isUserLogo?: boolean;
 }
 
-export const Logo = ({ text, className }: Props) => {
-  return (
-    <div className={styles.logo}>
-      <div>
-        <p className={className || ""}>{text}</p>
+export const Logo = ({ text, className, isUserLogo = false }: Props) => {
+  if (isUserLogo) {
+    return (
+      <div className={styles.logoUserMenu}>
+        <div>
+          <p className={className || ""}>{text}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={styles.logo}>
+        <div>
+          <p className={className || ""}>{text}</p>
+        </div>
+      </div>
+    );
+  }
 };
